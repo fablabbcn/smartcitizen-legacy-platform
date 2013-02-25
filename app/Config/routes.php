3ec1d4d6af5d3417b56abe98e747a1dc0bbd2637
adrelanex <adrelanex@gmail.com>
2013-02-25 15:42:11 +0100

    Added the config file (blided)

---
+++ app/Config/routes.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Routes configuration
+ *
+ * In this file, you set up routes to your controllers and their actions.
+ * Routes are very important mechanism that allows you to freely connect
+ * different urls to chosen controllers and their actions (functions).
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.Config
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+ /**
+ *Activate rss extensiona and layout
+ */
+	Router::parseExtensions('rss');
+/**
+ * Here, we are connecting '/' (base path) to controller called 'Pages',
+ * its action called 'display', and we pass a param to select the view file
+ * to use (in this case, /app/View/Pages/home.ctp)...
+ */
+	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+/**
+ * ...and connect the rest of 'Pages' controller's urls.
+ */
+	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+
+	Router::mapResources('feeds');
+	Router::mapResources('users');
+	Router::mapResources('posts');
+	Router::parseExtensions();
+	
+/**
+ * Route for the media Plugin (grant access to image resize) 
+ */
+	Router::connect('/img/:file_:format.:ext', array('controller' => 'medias', 'action' => 'crop', 'plugin' => 'Media' ),array(
+		'file' => '(Post|User)\/[^\/]+',
+		'format' => '50x50|100x100|150x150|200x150|300x300',
+		'ext' => 'jpg|png'
+	));
+/**
+ * Load all plugin routes.  See the CakePlugin documentation on 
+ * how to customize the loading of plugin routes.
+ */
+	CakePlugin::routes();
+
+/**
+ * Load the CakePHP default routes. Remove this if you do not want to use
+ * the built-in default routes.
+ */
+	require CAKE . 'Config' . DS . 'routes.php';
+
+
+ 
